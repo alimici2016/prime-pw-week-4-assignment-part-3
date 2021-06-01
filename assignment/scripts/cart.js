351c67f5c07c6522e8ce3d448cdfd865b925edc4
Hamsa Alimci <alimici2016@gmail.com>
2021-06-01 11:36:15 -0500

    Cart

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = ['item']
+function addItem(item) {
+return true;
+}
+
+function listItems(){
+for (items of basket){
+  console.log(items);
+}
+console.log(listItems());
+}
+function empty(){
+
+}
