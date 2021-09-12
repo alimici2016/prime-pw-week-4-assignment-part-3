a4101af8358178035b4016433ce722aa294dd472
alimici2016 <78386559+alimici2016@users.noreply.github.com>
2021-09-12 14:33:42 -0500

    Update cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,17 +2,26 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket = ['item']
+let basket = []
 function addItem(item) {
+  basket.push (item)
 return true;
 }
+addItem('apple');
+console.log('Should add item to bastket', basket);
 
 function listItems(){
-for (items of basket){
-  console.log(items);
+for (let i=0; i < basket.length; i++){
+console.log(basket[i]);
 }
-console.log(listItems());
 }
-function empty(){
+addItem('lemons');
+console.log('Should add lemon to basket', basket);
+console.log('Should loop items:'); listItems();
 
+function empty() {
+  basket.splice(0, basket.length)
 }
+console.log('Should empty Array:' )
+empty();
+console.log(basket);
